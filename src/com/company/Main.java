4a24ccf0940867f24e2a143f07fa7b8040e1890e
Renzo Santillán Chavez <rsantillanc@gmail.com>
2022-08-31 19:27:36 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        helloWorld();
+    }
+    
+    private static void helloWorld(){
+        System.out.println("Hello github!");
+    }
+}
